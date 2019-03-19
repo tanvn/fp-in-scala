3211a8f18cfba9ca7339f0eb9f168c52308e3be3
Tan Vu <tan.vu@bizreach.co.jp>
2019-03-20 01:59:49 +0900

    add list

---
+++ exercises/src/chapter3/List.scala
@@ -0,0 +1,19 @@
+package chapter3
+
+sealed trait List[+A]
+case object Nil extends List[Nothing]
+
+case class Cons[+A](head: A, tail: List[A]) extends List[A]
+
+object List {
+  def sum(ints: List[Int]): Int = ints match {
+    case Nil         => 0
+    case Cons(x, xs) => x + sum(xs)
+  }
+
+  def product(ds: List[Double]): Double = ds match {
+    case Nil          => 1.0
+    case Cons(0.0, _) => 0.0
+    case Cons(x, xs)  => x * product(xs)
+  }
+}
