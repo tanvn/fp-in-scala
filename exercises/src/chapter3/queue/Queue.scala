20697ed494fcd5cfe2004017616cfa70e120cfea
Tan Vu <tan.vu@bizreach.co.jp>
2019-04-20 23:48:18 +0900

    add Queue implementation

---
+++ exercises/src/chapter3/queue/Queue.scala
@@ -0,0 +1,36 @@
+package chapter3.queue
+
+trait Queue[T] {
+
+  def isEmpty: Boolean
+  def enQueue(t: T): Queue[T]
+  //Removes the element at the beginning of the immutable queue, and returns the new queue.
+  def deQueue(): Queue[T]
+  def head: Option[T]
+}
+
+private case class Node[T](value: Option[T], next: Option[Node[T]])
+    extends Queue[T] {
+
+  def empty[T]: Node[T] = Node(None, None)
+  override def isEmpty: Boolean = value.isEmpty
+
+  override def enQueue(t: T): Node[T] =
+    if (isEmpty) Node(Some(t), None) else Node(Some(t), Some(this))
+
+  override def head: Option[T] = value
+  override def deQueue(): Node[T] = next.getOrElse(empty)
+
+}
+
+object Queue {
+  def empty[T]: Queue[T] = Node(None, None)
+
+//  def main(args: Array[String]): Unit = {
+//
+//    val initQueue = Queue.empty[Int]
+//    val add10 = initQueue.enQueue(10)
+//    println(add10)
+//  }
+
+}
