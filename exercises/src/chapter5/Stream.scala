011790c9b72adc0a6593b33571273075d4b6c719
Tan Vu <tan.vu@bizreach.co.jp>
2019-05-01 17:18:55 +0900

    add take and skip

--- exercises/src/chapter5/Stream.scala
+++ exercises/src/chapter5/Stream.scala
@@ -3,11 +3,19 @@ package chapter5
 sealed trait Stream[+A] {
   def toList: List[A]
   def toListWithTailRec: List[A]
+
+  def take(n: Int): Stream[A]
+  def drop(n: Int): Stream[A]
+
 }
 
 case object Empty extends Stream[Nothing] {
   override def toList: List[Nothing] = List.empty[Nothing]
   def toListWithTailRec: List[Nothing] = List.empty[Nothing]
+
+  override def take(n: Int): Stream[Nothing] = Empty
+
+  override def drop(n: Int): Stream[Nothing] = Empty
 }
 
 case class Cons[+A](h: () => A, t: () => Stream[A]) extends Stream[A] {
@@ -22,6 +30,39 @@ case class Cons[+A](h: () => A, t: () => Stream[A]) extends Stream[A] {
     }
     go(this, List.empty)
   }
+
+  override def take(n: Int): Stream[A] =
+    if (n <= 0) this
+    else
+      this match {
+        case Cons(hd, tl) if n > 1 =>
+          Cons(hd, () => tl().take(n - 1))
+        case Cons(hd, _) if n == 1 =>
+          Cons(hd, () => Stream.empty[A])
+
+      }
+
+  def takeReverse(n: Int): Stream[A] = {
+
+    @annotation.tailrec
+    def go(remain: Int, acc: Stream[A], origin: Stream[A]): Stream[A] =
+      if (remain == 0) acc
+      else
+        origin match {
+          case Empty        => acc
+          case Cons(hd, tl) => go(remain - 1, Cons(hd, () => acc), tl())
+        }
+    go(n, Empty, this)
+  }
+
+  override def drop(n: Int): Stream[A] =
+    if (n <= 0) this
+    else
+      this match {
+        case Cons(_, tl) if n > 1  => tl().drop(n - 1)
+        case Cons(_, tl) if n == 1 => tl()
+
+      }
 }
 
 object Stream {
@@ -30,14 +71,24 @@ object Stream {
     lazy val tail = tl
     Cons(() => head, () => tail)
   }
+
   def empty[A]: Stream[A] = Empty
 
   def apply[A](as: A*): Stream[A] =
     if (as.isEmpty) empty[A] else cons(as.head, apply(as.tail: _*))
 
   def main(args: Array[String]): Unit = {
+
     val myStream = Stream(1, 2, 3, 4, 5, 6, 7)
     println(myStream.toList)
     println(myStream.toListWithTailRec)
+
+    println(myStream.take(3).toListWithTailRec)
+    println(myStream.take(10).toListWithTailRec)
+    println(myStream.drop(4).toListWithTailRec)
+    println(myStream.drop(1).toListWithTailRec)
+    println(myStream.drop(12).toListWithTailRec)
+    println(myStream.drop(0).toListWithTailRec)
+
   }
 }
