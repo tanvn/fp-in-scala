f198831db7f973b129dfeee559ddb002468b44bd
Tan Vu <tan.vu@bizreach.co.jp>
2019-06-14 23:25:34 +0900

    resolve conflict

--- exercises/src/chapter6/RNG.scala
+++ exercises/src/chapter6/RNG.scala
@@ -3,5 +3,6 @@ package chapter6
 trait RNG {
   def nextInt: (Int, RNG)
   def nonNegativeInt: (Int, RNG)
+  def nextStr: (String, RNG)
 
 }
--- exercises/src/chapter6/SimpleRNG.scala
+++ exercises/src/chapter6/SimpleRNG.scala
@@ -13,6 +13,11 @@ case class SimpleRNG(seed: Long) extends RNG {
     if (n > Int.MinValue) (Math.abs(n), nextRNG)
     else nonNegativeInt
   }
+
+  override def nextStr: (String, RNG) = {
+    val (num, rng) = nextInt
+    (s"$num v", rng)
+  }
 }
 
 object SimpleRNG {
@@ -21,6 +26,46 @@ object SimpleRNG {
     val (i, r) = rng.nextInt
     (if (i < 0) -(i + 1) else i, r)
   }
+
+  def ints(count: Int)(rng: RNG): (List[Int], RNG) = {
+    (1 to count).foldRight(List.empty[Int], rng)(
+      (_: Int, a: (List[Int], RNG)) => {
+        val (nextInt, nextRNG) = a._2.nextInt
+        (a._1 ::: nextInt :: Nil, nextRNG)
+      }
+    )
+
+  }
+  type Rand[+A] = RNG => (A, RNG)
+  val intRand: Rand[Int] = _.nextInt
+  // intRand is equivalent to the below (just a sugar syntax of scala using underscore)
+  val intRand2: Rand[Int] = (rng) => rng.nextInt
+
+  val stringRand: Rand[String] = _.nextStr
+
+  //  val doubleRand : Rand[Double] = (rng) => {
+  //    rng.nextInt * 1.5
+  //  }
+
+  def unit[A](a: A): Rand[A] =
+    rng => (a, rng)
+
+  def map[A, B](s: Rand[A])(f: A => B): Rand[B] =
+    (rng: RNG) => {
+      val (a, rng2: RNG) = s(rng)
+      (f(a), rng2)
+    }
+
+  def map2[A, B, C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] =
+    (rng: RNG) => {
+      //    val a3 : A = _
+      //    val res: (A, RNG) = ra(a3)
+      val (newA, rngA2: RNG) = ra(rng)
+      val (newB, _: RNG) = rb(rng)
+
+      (f(newA, newB), rngA2)
+
+    }
 
   def main(args: Array[String]): Unit = {
     val (n, nextRNG) = SimpleRNG(42).nextInt
@@ -33,6 +78,19 @@ object SimpleRNG {
     val (n4, nextRNG4) = nextRNG3.nonNegativeInt
 
     println(n4)
+    val seed = SimpleRNG(2000)
+    println(ints(10)(seed))
+    println(map(intRand)(i => s"value $i")(seed))
+
+    val map2Test = map2(intRand, stringRand)((a, b) => s"$a vs $b")
+    var newSeed: RNG = seed
+    for (_ <- 1 to 10) {
+
+      var res = map2Test(newSeed)
+      println(res._1)
+      newSeed = res._2
+
+    }
 
   }
 }
